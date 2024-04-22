867153287ef91ad5b65e9e1dd23ca388f4c473ca
Rami <rami.toma99@gmail.com>
2024-04-23 01:11:35 +0200

    new stuff

--- Stuff.java
+++ Stuff.java
@@ -1,12 +1,21 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Stuff {
-    public static void main (String[] args){
+    public static void main (String[] args) throws FileNotFoundException {
         Business iceShop = new Business("IceLels", 12345);
         iceShop.getBusiName();
         iceShop.getBusiNumber();
+
+        FileCreator saveBusi = new FileCreator();
+        saveBusi.createFile("lel.txt", 32434, "this is the first text in the function");
+
+        FileReader readFile = new FileReader();
+        readFile.readFile("lel.txt");
     }
 }
 
@@ -28,11 +37,26 @@ class Business {
 }
 
 class FileCreator {
-    public void createFile (){
+    public void createFile (String fileName, int busiNumber, String fileText){
         try {
-            FileWriter makeFile = new FileWriter("busiName.txt");
+            FileWriter makeFile = new FileWriter(fileName);
+            makeFile.write("this is the number of the busi: " + busiNumber + "\n");
+            makeFile.write("this is the text: " + fileText + "\n");
+            makeFile.close();
         } catch (IOException e){
             System.out.println("An error while writing file has happend.");
         }
+    }
+}
+
+class FileReader {
+    public void readFile(String fileName) throws FileNotFoundException {
+        File readFile = new File(fileName);
+        Scanner fileReader = new Scanner(readFile);
+        while (fileReader.hasNextLine()){
+            String line = fileReader.nextLine();
+            System.out.println("The file contains: " + line);
+        }
+
     }
 }
